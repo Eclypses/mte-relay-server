cf8e9c8161e4e0347b126354cbd4f339f78f37a7
Trevor Blackman <trevor.blackman@eclypses.com>
2024-03-03 05:23:26 -0700

    Update cache-adapters and settings-adapters

--- examples/cache-adapters/redis.js
+++ examples/cache-adapters/redis.js
@@ -11,7 +11,7 @@ module.exports = async function () {
     takeState: async function (id) {
       const value = await redisClient.get(id);
       /**
-       * Encoder State must be locked or removed from cache when in use
+       * Encoder State must be removed from cache when in use
        *  - Prevents the same encoder state from being used more than once.
        * This is not true for decoders; their state should be left in cache even when in use.
        */
@@ -21,7 +21,9 @@ module.exports = async function () {
       return value;
     },
     saveState: async function (id, state) {
-      await redisClient.set(id, state);
+      await redisClient.set(id, state, {
+        EX: 86400, // expires in 24 hours (in seconds)
+      });
     },
   };
 };
